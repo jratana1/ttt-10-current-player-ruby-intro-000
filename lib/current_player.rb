56d61f551934c943a10aaddd0223f4619e8b21fb
John Ratanaprasatporn <jratana1@gmail.com>
2020-09-03 22:48:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,8 @@ def turn_count(board)
   board.each do |input|
     counter = 0
     if input == 'X' || input == 'O'
-      return counter += 1
+      counter += 1
     end
+    counter
   end
   end
